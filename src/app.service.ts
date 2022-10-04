ab5f92d6c7e03a249a2735f7834eda0e9161c418
kels7 <hiketabo@gmail.com>
2022-10-04 17:14:49 +0000

    update function names and do not return user in a custom object

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProposalEntity } from './entities/proposal.entity'
 import { UserEntity } from './entities/user.entity'
-import { UserVoteEntity } from './entities/userVote.entity'
 
 @Injectable()
 export class ProposalService {
@@ -14,16 +13,23 @@ export class ProposalService {
     return await ProposalEntity.find({ where: { state: "open" } })
   }
 
-  async getCloseProposals(): Promise<any> {
+  async getConcludedProposals(): Promise<any> {
     return await ProposalEntity.find({ where: { state: "concluded" } })
+  }
+
+  async getVerifiedProposals(): Promise<any> {
+    return await ProposalEntity.find({ where: { verified: true } })
+  }
+
+  async getLatestProposal(): Promise<any> {
+    return await ProposalEntity.find({
+      order: {proposal_id: "DESC"}, 
+      take: 1
+    })
   }
 
   async getProposal( id: number): Promise<any> {
     return await ProposalEntity.find({ where: { proposal_id: id } })
-    // return await ProposalEntity.find({
-    //   order: {proposal_id: "DESC"}, 
-    //   take: limit
-    // })
   }
 
 }
@@ -31,19 +37,11 @@ export class ProposalService {
 @Injectable()
 export class UserService {
   async getUser(vk: string): Promise<object> {
-    const entity = await UserEntity.findOne({ where: { vk : vk}});
-    return {
-      vk: entity.vk,
-      ballot_idx: entity.ballot_idx,
-      choice_idx: entity.choice_idx,
-      proposals: entity.proposals
-      
-    }
+    return await UserEntity.find({ where: { vk : vk}});
   }
 
   async getUsers(): Promise<object> {
-    
-    return await UserEntity.find()
+    return await UserEntity.find();
   }
 }
 
