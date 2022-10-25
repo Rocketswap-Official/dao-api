db7eb7ebc5ca6aed28619e6784cf1943223ab0e8
cloakdkilla <getrektwithme1@gmail.com>
2022-10-26 00:14:05 +0100

    created button component

---
+++ ui/src/components/button/button.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let act: any;
+	export let style: T_ButtonStyle = ""; // primary / secondary
+
+	type T_ButtonStyle = "primary" | "secondary" | ""
+	type T_ButtonIcon = "arrow-right" | "";
+</script>
+
+<div>
+	<button on:click={act} class="connect outlined white {style}">
+		<div>
+			<slot />
+		</div>
+	</button>
+</div>
+
+<style>
+	button {
+		margin-top: var(--units-1vw);
+	}
+
+	button.icon {
+		margin: 0 auto;
+		padding: var(--units-1vw) var(--units-1_4vw);
+		border-radius: 100%;
+	}
+	button.icon:hover {
+		background-color: var(--panel-background-highlight);
+	}
+	button {
+		/* width: 50%; */
+		margin: var(--units-1vw) auto 0;
+	}
+	button > div {
+		padding-left: var(--units-1vw);
+		padding-right: var(--units-1vw);
+	}
+	@media (max-width: 480px) {
+	}
+</style>
