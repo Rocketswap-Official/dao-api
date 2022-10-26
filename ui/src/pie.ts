57749d963eeceda84e7a254a97081d42f359a4d6
kels7 <hiketabo@gmail.com>
2022-10-26 17:41:19 +0000

    add some config for pie chart

---
+++ ui/src/pie.ts
@@ -0,0 +1,25 @@
+//labels
+export const pieLabels = ["A","B", "C", "D", "E"];
+
+//colours
+export const pieColours = [
+    'rgb(255, 99, 132)',
+    'rgb(54, 162, 235)',
+    'rgb(255, 205, 86)'
+];
+
+//configurations
+export const pieOptions = {
+    maintainAspectRatio: false,
+    elements: {
+        arc:{
+            borderWidth: 0
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+
+};
