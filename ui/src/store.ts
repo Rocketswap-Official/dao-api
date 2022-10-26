d4a35c7500aec14304f0275008bc7e2a26c6dab7
kels7 <hiketabo@gmail.com>
2022-10-26 17:41:51 +0000

    add store for users data and wallet

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -1,4 +1,6 @@
 import { writable, type Writable } from "svelte/store";
-import type { I_Proposal } from "./types/imported-types"
+import type { I_Proposal, I_User } from "./types/imported-types"
 
 export const proposals_store: Writable<I_Proposal[]> = writable([])
+export const users_store: Writable<I_User[]> = writable([])
+export const wallet_store: Writable<any> = writable(null)
