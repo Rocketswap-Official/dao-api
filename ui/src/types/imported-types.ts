a545b6483a3596808252857268cb3f6e9ed0f87c
kels7 <hiketabo@gmail.com>
2022-10-26 17:45:46 +0000

    correct and add user interface

--- ui/src/types/imported-types.ts
+++ ui/src/types/imported-types.ts
@@ -1,14 +1,26 @@
 export interface I_Proposal {
-    id: number;
     proposal_id: number;
     title: string;
     description: string;
     date_decision: { __time__: string };
-    choices: [];
+    choices: any[];
     state: string;
     ballot_count: number;
     counted: string;
     verified: string;
     lp_weight: number;
-    results: {};
+    results: any;
+}
+
+export interface I_User {
+    vk?: string;
+    ballot_idx?: number[];
+    choice_idx?: number[]
+    weight?: number[]
+    proposals?: number[]
+    rswp_balance: number;
+    rocket_fuel: number;
+    staked_rswp: number;
+    staked_lp_value?: number[];
+    lp_value?: number[];
 }
