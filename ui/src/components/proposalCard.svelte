a80316be1fb27abd3fdee71453b2d3650af43c5e
kels7 <hiketabo@gmail.com>
2022-10-26 17:44:01 +0000

    add proposal card component

---
+++ ui/src/components/proposalCard.svelte
@@ -0,0 +1,132 @@
+<!--div>proposal card </div-->
+<!--div>we will need a progress bar component </div-->
+<script lang="ts">
+    
+    import ProgressBar from './choiceSelectorPieChart.svelte';
+    import  type { I_Proposal, I_User }  from '../types/imported-types';
+    export let proposal: I_Proposal;
+    export let user: I_User[];
+
+    let results = proposal.results
+    let choices = proposal.choices
+    let total = 0;
+    let choiceArray: any[] = [];
+
+    for (let s of choices){
+        let i = choices.indexOf(s)
+        choiceArray.push({"proposalId": proposal.proposal_id, "choiceIdx": i, "choice": s, "voteWeight": 0.00});
+    }
+    //console.log(label)
+
+    if (Object.keys(user).length > 0){
+        for (let u of user){
+            for (let ui of u.proposals){
+                if (ui === proposal.proposal_id){
+                    let indx = u.proposals.indexOf(ui);
+                    let c = u.choice_idx[indx];
+                    let cWeight = u.rswp_balance + u.rocket_fuel + u.staked_rswp + u.staked_lp_value[indx] + u.lp_value[indx];
+                    for (let u1 of choiceArray){
+                        if (u1.choiceIdx === c){
+                            u1.voteWeight += cWeight
+                            u1.vk = u.vk;
+                            
+                        }
+                    }
+
+                    total += cWeight;
+                }
+
+
+            }
+            
+        }
+        for (let u2 of choiceArray){
+            let w = u2.voteWeight;
+            
+            u2.voteWeight = (w/total*100).toFixed(2);
+            if(w === 0 || total === 0){
+                u2.voteWeight = parseFloat("0.00");
+            }
+        }
+    }
+
+    //check for verified state here
+    if(proposal.verified === 'true'){
+        const choice_idx = Object.keys(results)
+
+        for (let idx of choice_idx){
+            total += results[idx]; //could be an object with __fixed__ key
+        }
+        for (let u0 of choiceArray){
+            
+            let w = u0.voteWeight;
+            u0.voteWeight = (w/total*100).toFixed(2) + "%";
+            if(w === 0 || total === 0){
+                u0.voteWeight = "0.00%" 
+                
+            }
+            
+        }
+
+    }   
+    
+</script>
+
+
+<div class="container panel">
+    <div class="proposal-title">#{proposal.proposal_id} - {proposal.title}</div>
+    <div class="metadata-container">
+        <div>Status: <span class="text-green" >{proposal.state}</span></div>
+        <div>Voting Ends: {proposal.date_decision.__time__}</div>
+    </div>
+
+    <!--div>Description: 
+        <h4 >
+            {proposal.description}
+        </h4>
+    </div-->
+
+    <div class="choice-container">
+        
+            
+        <ProgressBar choices={choiceArray} />
+            
+    </div>
+    
+
+    <div class="stats-container">
+        <div>Total votes: {(total/102000321*100).toFixed(8)}%</div>
+        <div>{total}/102000321 RSWP</div>
+    </div>
+</div>
+
+<style>
+    .container{
+        width: 60%;
+        padding: 3vw;
+        box-shadow: var(--panel-box-shadow-higher);
+        margin-bottom: 15px;
+        color: var(--font-primary-color);
+        font-weight: 300;
+    }
+    .proposal-title {
+        font-size: var(--units-1_14vw);
+        font-weight: 500;
+
+    }
+    .metadata-container{
+        margin-bottom: 3vw;
+        font-size: var(--units-09vw);
+    }
+    .choice-container{
+        margin-bottom: 3vw;
+        width: 100%;
+        overflow-x: hidden;
+        /* margin: 10px 0 10px 0; */
+    }
+    .stats-container{
+        margin-top: 10px;
+        font-size: 11px;
+    }
+   
+</style>
