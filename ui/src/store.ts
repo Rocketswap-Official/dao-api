ec7e06b50b72aeda4fd7baa7ac2634f8650504bf
kels7 <hiketabo@gmail.com>
2022-11-01 13:01:51 +0000

    add toast data store for access to toast component

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -4,3 +4,8 @@ import type { I_Proposal, I_User } from "./types/imported-types"
 export const proposals_store: Writable<I_Proposal[]> = writable([])
 export const users_store: Writable<I_User[]> = writable([])
 export const wallet_store: Writable<any> = writable(null)
+//Toast
+export const showToast = writable(false)
+export const title = writable('')
+export const message = writable('')
+export const errorMessage = writable('')
