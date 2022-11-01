2ea63e9a36ce0f5b09fdd39b5b0121ca1039ba1c
kels7 <hiketabo@gmail.com>
2022-11-01 23:11:26 +0000

    use object to capture toast data

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -1,5 +1,5 @@
 import { writable, type Writable } from "svelte/store";
-import type { I_Proposal, I_User } from "./types/imported-types"
+import type { I_Proposal, I_User, I_Toast } from "./types/imported-types"
 
 export const proposals_store: Writable<I_Proposal[]> = writable([])
 export const users_store: Writable<I_User[]> = writable([])
@@ -9,7 +9,8 @@ export const modal_open_store: Writable<any> = writable(false)
 export const modal_data_store: Writable<any> = writable({})
 export const modal_callback: Writable<Function | false> = writable()
 //Toast
-export const showToast = writable(false)
-export const title = writable('')
-export const message = writable('')
-export const errorMessage = writable('')
+export const showToast: Writable<any> = writable(false)
+export const toast_data: Writable<I_Toast> = writable({})
+// export const title = writable('')
+// export const message = writable('')
+// export const errorMessage = writable('')
