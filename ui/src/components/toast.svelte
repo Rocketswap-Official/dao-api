e6814e41d121002c76c990601970a98c9e1bcb51
kels7 <hiketabo@gmail.com>
2022-11-01 13:00:00 +0000

    add toast toast component

---
+++ ui/src/components/toast.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    //import some icons
+
+    export let title: string;
+    export let message: string;
+    export let errorMessage: string;
+    export let showToast: boolean = false;
+</script>
+
+{#if showToast}
+    <div class="toast-container {errorMessage?'error':'success'}">
+        
+        <div class="times" on:click={()=>showToast=false}>&times</div>
+        
+        <h3 class="title">
+            {title}
+        </h3>
+        <div class="message-area">
+            {#if errorMessage}
+                {errorMessage}
+            {:else}
+                {message}
+            {/if}
+        </div>
+        
+    </div>
+        
+{/if}
+
+<style>
+    .toast-container {
+        position: fixed;
+        width: 250px;
+        border-radius: 5px;
+        padding: 0.5em 1em 3.5em 1em;
+    }
+    .times{
+        position: absolute;
+        top: 0;
+        font-size: 24px;
+        font-weight: 850;
+        right: 0.3em;
+        cursor: pointer;
+    }
+    .title {
+        font-weight: 800;
+        font-size: var(--units-1_2vw);
+    }
+    .message-area{
+        font-size: var(--units-1vw);
+    }
+    .success {
+        background-color: rgb(62,158,100);
+    }
+    .error {
+        background-color: rgb(234,76,28);
+    }
+</style>
