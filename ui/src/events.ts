95506ae57d151ba265303fb9a5f52c1a6ff7742b
kels7 <hiketabo@gmail.com>
2022-11-04 00:14:46 +0000

    add modal open function

--- ui/src/events.ts
+++ ui/src/events.ts
@@ -5,3 +5,8 @@ export function handle_modal_close() {
     modal_data_store.set({})
     modal_callback.set(false)
 }
+export function handle_modal_open() {
+    modal_open_store.set(true)
+    modal_data_store.set({})
+    modal_callback.set(false)
+}
