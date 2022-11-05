77b211fd2abdb4f318c1694d834ee9a583dc3282
kels7 <hiketabo@gmail.com>
2022-11-05 14:45:21 +0000

    update

--- ui/src/types/imported-types.ts
+++ ui/src/types/imported-types.ts
@@ -1,10 +1,11 @@
 export interface I_Proposal {
-    id: number;
+    id?: number;
     proposal_id: number;
     title: string;
     description: string;
     date_decision: { __time__: string };
-    choices: I_Choice[];
+    //choices: I_Choice[];
+    choices: string[];
     state: string;
     ballot_count: number;
     counted: string;
@@ -13,17 +14,24 @@ export interface I_Proposal {
     results: {};
 }
 
-export interface I_Choice { choice: string, choiceIdx: number, proposalId: number, vk: string, voteWeight: number }
+export interface I_Choice { 
+    choice: string, 
+    choiceIdx: number, 
+    proposalId: number, 
+    vk?: string, 
+    voteWeight: number, 
+    total: number 
+}
 
 export interface I_User {
-    vk?: string;
-    ballot_idx?: number[];
-    choice_idx?: number[]
-    weight?: number[]
-    proposals?: number[]
+    vk: string;
+    ballot_idx: number[];
+    choice_idx: number[]
+    weight: number[]
+    proposals: number[]
     rswp_balance: number;
     rocket_fuel: number;
     staked_rswp: number;
-    staked_lp_value?: number[];
-    lp_value?: number[];
+    staked_lp_value: number[];
+    lp_value: number[];
 }
