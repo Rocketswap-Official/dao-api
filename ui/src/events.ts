3284e63bf88f545d260c8d5d9ec797f017506b37
kels7 <hiketabo@gmail.com>
2022-11-07 23:43:29 +0000

    add modal index store to function

--- ui/src/events.ts
+++ ui/src/events.ts
@@ -1,12 +1,13 @@
-import { modal_open_store,modal_callback, modal_data_store  } from "./store"
+import { modal_open_store, modal_index_store, modal_callback, modal_data_store  } from "./store"
 
 export function handle_modal_close() {
     modal_open_store.set(false)
     modal_data_store.set({})
     modal_callback.set(false)
 }
-export function handle_modal_open() {
+export function handle_modal_open_voting() {
     modal_open_store.set(true)
+    modal_index_store.set(1)
     modal_data_store.set({})
     modal_callback.set(false)
 }
