b8adc53fe64be36ef986ebedc3b99f1d05b08503
kels7 <hiketabo@gmail.com>
2022-11-15 21:40:23 +0000

    add config

---
+++ ui/src/config.ts
@@ -0,0 +1,7 @@
+export const connectionRequest = {
+    appName: 'My Killer dApp',
+    version: '1.0.0', 
+    logo: 'images/logo.png', 
+    contractName: 'con_killer_app', 
+    networkType: 'testnet'
+}
