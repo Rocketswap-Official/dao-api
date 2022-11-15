757f686783bf78fc5f49f8397f6590771d00f43d
kels7 <hiketabo@gmail.com>
2022-11-15 21:40:45 +0000

    update with lwc store

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -3,7 +3,7 @@ import type { I_Proposal, I_User, I_Choice } from "./types/imported-types"
 
 export const proposals_store: Writable<I_Proposal[]> = writable([])
 export const users_store: Writable<I_User[]> = writable([])
-export const wallet_store: Writable<any> = writable(null)
+export const wallet_store: Writable<string> = writable("")
 
 export const modal_open_store: Writable<any> = writable(false)
 export const modal_data_store: Writable<any> = writable({})
@@ -11,7 +11,10 @@ export const modal_index_store: Writable<number> = writable(0)
 export const modal_callback: Writable<Function | false> = writable()
 //Toast
 // export const showToast: Writable<any> = writable(false)
-// export const toast_data: Writable<I_Toast> = writable({})
+export const toast_store: Writable<any> = writable({show: false})
 
 //ChoiceArray
 export const choice_array_store: Writable<I_Choice[]> = writable([])
+
+//Lamden Wallet Controller
+export const lwc_store: Writable<any> = writable(null)
