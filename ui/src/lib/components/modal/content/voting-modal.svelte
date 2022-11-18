53bea65b96b2382bf40a327dc495c34d6a491df4
kels7 <hiketabo@gmail.com>
2022-11-18 03:52:29 +0000

    refactor + add button + add checkbox + check for selected choice

--- ui/src/lib/components/modal/content/voting-modal.svelte
+++ ui/src/lib/components/modal/content/voting-modal.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+
 
 
     import { wallet_store, choice_array_store, modal_data_store } from "../../../store"
@@ -7,29 +9,44 @@
     let idx = $modal_data_store;
     //console.log(idx)
     let choices = $choice_array_store[1]
+    
 
-    //console.log(choices)
-    let total: number;
+    let choiceOnly = []
+
+
+    for (let cc of choices){
+        if(cc.choice){
+            choiceOnly.push(cc.choice) 
+        }
+    }
 
     
     //get vk from lamden wallet
-    let vk = $wallet_store;
-    let vkVoted = false;
-    let chosenChoice: number;
-    let n = 0;
-    let weights: number[] = [];
+    //let vk = $wallet_store;
+    let vk = "cccccccccc";
+   
+    let c = 0
+
+    let selectOne;
+
+    onMount(()=>{
+        selectOne = (e: any)=>{
+            const box = e.target;
+            const group = document.getElementsByClassName("checkbox")
+            if(box.checked){
+                for (let ele of group){
+
+                    ele.checked = false;
+                }
+                box.checked = true;
+            }else{
+                box.checked = false;
+            }
+        }
+    })
+
     
 
-    for (let c of choices){
-        if (c.vk === vk){
-            vkVoted = true;
-            chosenChoice = c.choiceIdx;
-        }
-
-        weights.push(parseFloat(c.voteWeight));
-        total = c.total
-        n = n + 1
-    }
 
 </script>
 
@@ -37,73 +54,54 @@
 <div style="margin-top: 3vw; font-size: var(--units-1_14vw);">
     <ol type="A">
     {#each choices as choice}
-        {#if vkVoted}
-            {#if choice.choiceIdx === chosenChoice}
-                <label for="choice">
-                    <button class="voted" disabled>{choice.choice}</button>
-                </label>
-                <div class="progress-bar">
-                    <div class="track text-primary-color-dark weight-300" style="width: {choice.voteWeight}">{choice.voteWeight}</div>
-                </div>
-            {:else}
-                <label for="choice">
-                    <button disabled>{choice.choice}</button>
-                </label>
-                <div class="progress-bar">
-                    <div class="track text-primary-color-dark weight-300" style="width: {choice.voteWeight}">{choice.voteWeight}</div>
-                </div>
-            {/if}
+        
+    <!-- TODO: leave a little space between list bullet and border -->
+    <div class="choice mb-1 " class:voted={choice.vk === vk}>   
+        <li >
             
-        {:else}
-            <li class="mb-1" style="margin-left: var(--units-1_4vw); font-weight: 400">
+            <!--div class="flex row space-between">
+                <div style="width: 70%; overflow:hidden">{choice.choice}</div>
                 
-                <div class="flex row space-between">
-                    <div style="width: 70%; overflow:hidden; ">{choice.choice}</div>
-                    <div>{choice.voteWeight}%</div>
-                </div>
-                
+            </div-->
+            <div class="flex row space-between">
+                <label for="choice">{choice.choice}</label>
+                <input type="checkbox" bind:group={choiceOnly} value={choice.choiceIdx} class="checkbox" on:click={selectOne}/>
+            </div>
             
-                
-            </li>
-            
-        {/if}
+        </li>
+           
+    </div> 
     {/each}
     </ol>
     
 </div>
 
-
+<div class="flex row center">
+    <button class=" outlined primary white">
+        <div>Cast Vote</div>
+    </button>
+</div>
 
 <style>
+    li {
+        margin-left: var(--units-1_4vw); 
+        font-weight: 400; 
+        /* list-style-position: inside; */
+    }
 
-    .progress-bar{
-        width: 100%;
-        height: 14px;
-        padding: 0;
-        margin: 5px 0 5px 0;
-    }
-    .track{
-        background-color: aqua;
-        border-radius: 3px;
-        text-align: center;
-        color: var(--background-color);
-        padding: 0;
-    }
-    label{
-        font-size: 11px;
-    }
-    button{
-        padding: 4px;
-        background-color: transparent;
-        border: 0.5px solid grey;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-    .voted {
-        border: 2px solid seagreen;
-    }
     ol {    
         padding-left: 0;
+    }
+    .voted {
+        background-color: var(--background-color); 
+        padding: 0.7em 0 0.7em 0; 
+    }
+    .choice {
+        padding: 0.7em 0 0.7em 0; 
+        border: 3px solid var(--background-color);
+    }
+    .checkbox {
+        background-color: green;
     }
     
 </style>
