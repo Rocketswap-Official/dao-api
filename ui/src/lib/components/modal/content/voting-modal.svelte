ce86d11291c769da3604517a3392e142ade5f024
kels7 <hiketabo@gmail.com>
2022-11-19 12:40:40 +0000

    add useful imports+move some func to sever-side

--- ui/src/lib/components/modal/content/voting-modal.svelte
+++ ui/src/lib/components/modal/content/voting-modal.svelte
@@ -1,52 +1,63 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-
-
-
-    import { wallet_store, choice_array_store, modal_data_store } from "../../../store"
+    import { sendTransaction } from '../../../../funcs'
+    import { ballotTxnInfo } from '../../../../config'
+    import { getCheckBoxGroup } from './voting-modal.utils'
+    import { 
+        wallet_store, 
+        choice_array_store, 
+        modal_data_store, 
+        lwc_store, 
+        toast_store } from "../../../store"
 	//import type { I_Choice } from "src/types/imported-types";
-
     let idx = $modal_data_store;
-    //console.log(idx)
-    let choices = $choice_array_store[1]
-    
 
-    let choiceOnly = []
-
-
-    for (let cc of choices){
-        if(cc.choice){
-            choiceOnly.push(cc.choice) 
-        }
-    }
-
-    
-    //get vk from lamden wallet
+    let choices: any = $choice_array_store[idx]
+    let group: any = []
     //let vk = $wallet_store;
     let vk = "cccccccccc";
-   
-    let c = 0
+    let selectOne: any;
 
-    let selectOne;
+    group = getCheckBoxGroup(choices, group)
+    
+    
+    const submitSelectedChoice = ()=>{
+        toast_store.set({show: true, title:"Transacton State", pending:true, message:"Pending"})
+
+        sendTransaction($lwc_store, ballotTxnInfo)
+        console.log(ballotTxnInfo)
+        
+    }
+
+    const changeBackgroundColor = (e: any)=>{
+            console.log(e.target)
+            const background = e.target;
+            background.style.backgroundColor = "#0E1621";
+            background.style.padding = "0.7em 0 0.7em 0";
+    }
 
     onMount(()=>{
         selectOne = (e: any)=>{
             const box = e.target;
-            const group = document.getElementsByClassName("checkbox")
+            const group: any = document.getElementsByClassName("checkbox")
             if(box.checked){
                 for (let ele of group){
 
                     ele.checked = false;
                 }
                 box.checked = true;
+                
+              
             }else{
                 box.checked = false;
             }
+
+            box.checked?ballotTxnInfo.kwargs.choice_idx = parseInt(box.value):ballotTxnInfo.kwargs.choice_idx = "";
+            ballotTxnInfo.kwargs.proposal_idx = parseInt(box.name)
         }
+
+        
     })
-
-    
-
 
 </script>
 
@@ -56,16 +67,21 @@
     {#each choices as choice}
         
     <!-- TODO: leave a little space between list bullet and border -->
-    <div class="choice mb-1 " class:voted={choice.vk === vk}>   
+    <div class="choice mb-1 " class:voted={choice.vk === vk} on:click={changeBackgroundColor} on:keyup={changeBackgroundColor}>  
+        
         <li >
             
-            <!--div class="flex row space-between">
-                <div style="width: 70%; overflow:hidden">{choice.choice}</div>
-                
-            </div-->
             <div class="flex row space-between">
                 <label for="choice">{choice.choice}</label>
-                <input type="checkbox" bind:group={choiceOnly} value={choice.choiceIdx} class="checkbox" on:click={selectOne}/>
+                <input 
+                    type="checkbox" 
+                    class="checkbox"
+                    bind:group={group} 
+                    name={choice.proposalId}
+                    value={choice.choiceIdx} 
+                    on:click={selectOne}
+                    checked/>
+                    
             </div>
             
         </li>
@@ -77,7 +93,7 @@
 </div>
 
 <div class="flex row center">
-    <button class=" outlined primary white">
+    <button class=" outlined primary white" on:click={submitSelectedChoice}>
         <div>Cast Vote</div>
     </button>
 </div>
@@ -100,8 +116,16 @@
         padding: 0.7em 0 0.7em 0; 
         border: 3px solid var(--background-color);
     }
-    .checkbox {
-        background-color: green;
+    /* input[type=checkbox], input[type=checkbox]:checked {
+        -moz-appearance:none;
+        -webkit-appearance:none;
+        -o-appearance:none;
+    } */
+
+    input[type=checkbox] {
+        width: 20px;
+        height: 20px;
     }
+
     
 </style>
