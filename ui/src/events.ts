d6230aa9ffc6c3f6d5dd501f27facee650c52507
kels7 <hiketabo@gmail.com>
2022-11-19 12:35:01 +0000

    comment out setting modal_store

--- ui/src/events.ts
+++ ui/src/events.ts
@@ -2,12 +2,18 @@ import { modal_open_store, modal_index_store, modal_callback, modal_data_store  
 
 export function handle_modal_close() {
     modal_open_store.set(false)
-    modal_data_store.set({})
+    //modal_data_store.set({})
     modal_callback.set(false)
 }
 export function handle_modal_open_voting() {
     modal_open_store.set(true)
     modal_index_store.set(1)
-    modal_data_store.set({})
+    //modal_data_store.set({})
     modal_callback.set(false)
 }
+// export function handle_modal_open_voting() {
+//     modal_open_store.set(true)
+//     modal_index_store.set(1)
+//     modal_data_store.set({})
+//     modal_callback.set(false)
+// }
