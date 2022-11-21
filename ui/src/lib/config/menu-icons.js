224217cec35368511b4dad4eccf1b2e165d3dde4
kels7 <hiketabo@gmail.com>
2022-11-21 22:45:33 +0000

    change svg imports paths

--- ui/src/lib/config/menu-icons.js
+++ ui/src/lib/config/menu-icons.js
@@ -2,15 +2,13 @@
 import icon_home from '$lib/svg/menu/icon-home.svg'
 import icon_home_color from '$lib/svg/menu/icon-home-color.svg'
 import icon_create from '$lib/svg/menu/icon-create.svg'
-import icon_create_color from '$lib/svg/menu/icon-create-color.svg'
-import icon_voted from '$lib/svg/menu/icon-voted.svg'
-import icon_voted_color from '$lib/svg/menu/icon-voted-color.svg'
-import icon_help from '$lib/svg/menu/icon-help.svg'
-import icon_help_color from '$lib/svg/menu/icon-help-color.svg'
-
 export default {
-    icon_home, icon_home_color,
-    icon_create, icon_create_color,
-    icon_voted, icon_voted_color,
-    icon_help, icon_help_color
+    icon_home: "svg/menu/icon-home.svg", 
+    icon_home_color: "svg/menu/icon-home-color.svg",
+    icon_create: "svg/menu/icon-create.svg", 
+    icon_create_color: "svg/menu/icon-create-color.svg",
+    icon_voted: "svg/menu/icon-voted.svg", 
+    icon_voted_color: "svg/menu/icon-voted-color.svg",
+    icon_help: "svg/menu/icon-help.svg", 
+    icon_help_color: "svg/menu/icon-help-color.svg"
 }
