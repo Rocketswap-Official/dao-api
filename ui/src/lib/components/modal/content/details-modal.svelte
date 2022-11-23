0e1ad672a594eb48b7bef4d658ef5c8d3a603770
kels7 <hiketabo@gmail.com>
2022-11-23 13:17:03 +0000

    add details component

---
+++ ui/src/lib/components/modal/content/details-modal.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { modal_data_store, proposals_store } from '../../../store'
+    import { getProposalDescription } from './content-modal.utils'
+
+    let proposal_id = $modal_data_store.proposal_id
+    let description = getProposalDescription($proposals_store, proposal_id)
+    
+
+</script>
+
+<div style="margin-top: 2vw; font-size: var(--units-1_14vw);">
+    <h2 style="font-weight: 700;">Description</h2>
+    {description}
+</div>
