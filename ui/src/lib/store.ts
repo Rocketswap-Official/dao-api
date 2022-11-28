51478fb676b1d8bda8c4434303e7a490a3725ebe
kels7 <hiketabo@gmail.com>
2022-11-28 23:56:07 +0000

    add balance and rswp stores

--- ui/src/lib/store.ts
+++ ui/src/lib/store.ts
@@ -4,6 +4,7 @@ import type { I_Proposal, I_User, I_Choice } from "./types/imported-types"
 export const proposals_store: Writable<I_Proposal[]> = writable([])
 export const users_store: Writable<I_User[]> = writable([])
 export const wallet_store: Writable<string> = writable("")
+export const balances_store: Writable<any> = writable({TAU: 0, RSWP: 0})
 
 export const modal_open_store: Writable<any> = writable(false)
 export const modal_data_store: Writable<any> = writable({})
@@ -18,3 +19,6 @@ export const choice_array_store: Writable<I_Choice[]> = writable([])
 
 //Lamden Wallet Controller
 export const lwc_store: Writable<any> = writable(null)
+
+//RSWP approval 
+export const rswp_approval_store: Writable<number> = writable(0)
