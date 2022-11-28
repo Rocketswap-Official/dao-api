a819510fa08a0015689d42ca447ce21b909e03ee
kels7 <hiketabo@gmail.com>
2022-11-28 23:53:55 +0000

    add dao test contract

--- api/src/config.ts
+++ api/src/config.ts
@@ -4,9 +4,10 @@ export const config_prod = {
     staking_contract: "con_staking_rswp_rswp_interop_v2",
 	lp_staking_contract: "con_liq_mining_rswp_rswp",
     dex_native_token: "con_rswp_lst001",
-    dao_contract: "con_lite_dao",
+    dao_contract: "con_lite_dao_test",
     network_type: "mainnet",
-    block_service_urls: process.env.block_service_urls?.split(",") || ["165.22.47.195:3535"]
+    block_service_urls: process.env.block_service_urls?.split(",") || ["localhost:35993"]
+    //["blockservice.opticprotocol.finance"]
     //block_service_url: process.env.BLOCK_SERVICE_URL || "0.0.0.0:3535"
 };
 
