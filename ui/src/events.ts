11ecc20f87a00c193be4b5ccd9ed46dd6c308e97
kels7 <hiketabo@gmail.com>
2022-11-28 23:55:41 +0000

    add rswp approval open modal functions

--- ui/src/events.ts
+++ ui/src/events.ts
@@ -17,6 +17,12 @@ export function handle_modal_open_details() {
     //modal_data_store.set({})
     modal_callback.set(false)
 }
+export function handle_modal_open_approve() {
+    modal_open_store.set(true)
+    modal_index_store.set(3)
+    //modal_data_store.set({})
+    modal_callback.set(false) //THOUGHT: remove if default value is false in store.ts
+}
 // export function handle_modal_open_voting() {
 //     modal_open_store.set(true)
 //     modal_index_store.set(1)
