c6219c63a3234ef24535fc4a2de8ac0ad47bf4df
kels7 <hiketabo@gmail.com>
2022-11-29 00:04:18 +0000

    initialise from page.ts

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -4,9 +4,9 @@
     import ProposalCard from '$lib/components/proposalCard.svelte';
     import Button from '$lib/components/button/button.svelte'
     import { handle_modal_open_voting, handle_modal_open_details } from '../events'
-    import { proposals_store, choice_array_store} from '$lib/store';
+    //import { proposals_store, choice_array_store} from '$lib/store';
 
-
+    export let data: any;
     let n = 0;
     let it = 0;
     const increment = ()=>{
@@ -18,21 +18,19 @@
     }
 
     
-
-    
 </script>
 
 <div style="display: grid; 
     grid-template-columns:repeat(auto-fit, minmax(200px, 1fr)); 
     grid-gap: 50px">
     
-    {#if Object.keys($proposals_store).length > 0}
+    {#if Object.keys(data.proposals).length > 0}
         
-        {#each $proposals_store as proposal}
+        {#each data.proposals as proposal}
            
             <ProposalCard {proposal}> 
                 
-                <ChoiceSelectorPieChart choices ={$choice_array_store[increment()]}/>
+                <ChoiceSelectorPieChart choices ={data.choiceArray[increment()]}/>
 
                 <div class="flex row j-end" style="margin-top: 3vw;">
                     <div class="mr-1em">
