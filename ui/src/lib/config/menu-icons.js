7ca22ca917ac49780b03ad845bcf8ef02d8340e1
kels7 <hiketabo@gmail.com>
2023-04-04 11:44:44 +0000

    remove unused imports

--- ui/src/lib/config/menu-icons.js
+++ ui/src/lib/config/menu-icons.js
@@ -1,7 +1,4 @@
 
-import icon_home from '$lib/svg/menu/icon-home.svg'
-import icon_home_color from '$lib/svg/menu/icon-home-color.svg'
-import icon_create from '$lib/svg/menu/icon-create.svg'
 export default {
     icon_home: "svg/menu/icon-home.svg", 
     icon_home_color: "svg/menu/icon-home-color.svg",
