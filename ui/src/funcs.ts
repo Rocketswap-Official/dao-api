d5b2f39e561dafefb5121f3d17f787965571927b
kels7 <hiketabo@gmail.com>
2023-04-04 21:39:37 +0000

    delete unneeded module

--- ui/src/funcs.ts
+++
@@ -1,6 +0,0 @@
-import type { I_TransactionObj } from './lib/types/imported-types'
-
-export const sendTransaction = async (lwc, callback, transaction: I_TransactionObj): Promise<void> =>{
-    lwc.sendTransaction(transaction, callback) 
-    //console.log(transaction)
-}
