095a235d02c6f179e9ba598330fff72507d72651
kels7 <hiketabo@gmail.com>
2023-04-14 20:32:45 +0000

    create tuple type

--- ui/src/lib/types/imported-types.ts
+++ ui/src/lib/types/imported-types.ts
@@ -1,86 +1,87 @@
-import type { ComponentType } from "svelte";
+import type { ComponentType } from 'svelte';
+
+export type Tuple7<T> = [T, T, T, T, T, T, T];
 
 //########## Proposal Types ##############################
 
 export interface I_Proposal {
-    id?: number;
-    proposal_id: number;
-    title: string;
-    description: string;
-    date_decision: { __time__: string };
-    //choices: I_Choice[];
-    choices: string[];
-    state: string;
-    ballot_count: number;
-    counted: string;
-    verified: string;
-    lp_weight: number;
-    results: {};
+	id?: number;
+	proposal_id: number;
+	title: string;
+	description: string;
+	date_decision: { __time__: Tuple7<number> };
+	//choices: I_Choice[];
+	choices: string[];
+	state: string;
+	ballot_count: number;
+	counted: string;
+	verified: string;
+	lp_weight: number;
+	results: {};
 }
 
-export interface I_Choice { 
-    choice: string, 
-    choiceIdx: number, 
-    proposalId: number, 
-    vk?: string, 
-    voteWeight: number, 
-    total: number 
+export interface I_Choice {
+	choice: string;
+	choiceIdx: number;
+	proposalId: number;
+	vk?: string;
+	voteWeight: number;
+	total: number;
 }
 
 //########## User Types ##############################
 
 export interface I_User {
-    vk: string;
-    ballot_idx: number[];
-    choice_idx: number[]
-    weight: number[]
-    proposals: number[]
-    rswp_balance: number;
-    rocket_fuel: number;
-    staked_rswp: number;
-    staked_lp_value: number[];
-    lp_value: number[];
+	vk: string;
+	ballot_idx: number[];
+	choice_idx: number[];
+	weight: number[];
+	proposals: number[];
+	rswp_balance: number;
+	rocket_fuel: number;
+	staked_rswp: number;
+	staked_lp_value: number[];
+	lp_value: number[];
 }
 
 //########## Modal Types ##############################
 
 export interface I_ModalType {
-    type: string;
-    component: ComponentType | string;
+	type: string;
+	component: ComponentType | string;
 }
 
 //########## Transaction Types ##############################
 
 export interface I_ProposalInfo {
-    title: string;
-    description: string;
-    date_decision: string;
-    choices: I_ChoicesObj[]
+	title: string;
+	description: string;
+	date_decision: string;
+	choices: I_ChoicesObj[];
 }
 export interface I_ChoicesObj {
-    id: number;
-    text: string;
-
+	id: number;
+	text: string;
 }
-export interface I_TransactionObj{
-    contractName?: string;
-    senderVk?: string;
-    networkName: string;
-    networkType: string;
-    methodName: string; 
-    kwargs: any; 
-    stampLimit: number;
+export interface I_CountObj {
+	proposal_idx: number;
 }
-
+export interface I_TransactionObj {
+	contractName?: string;
+	senderVk?: string;
+	networkName: string;
+	networkType: string;
+	methodName: string;
+	kwargs: any;
+	stampLimit: number;
+}
 
 export interface I_BallotInfo {
-    proposal_idx: string|number;
-    choice_idx: string|number;
-    
+	proposal_idx: string | number;
+	choice_idx: string | number;
 }
 
 export interface I_RSWPApprovalInfo {
-    amount: number;
-    to: string;
-    
+	amount: number;
+	to: string;
 }
