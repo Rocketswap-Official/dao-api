0c0d446abf06278de470c74b72331121b36624a6
kels7 <hiketabo@gmail.com>
2023-04-14 09:35:00 +0000

    add icons

--- ui/src/lib/config/menu-icons.js
+++ ui/src/lib/config/menu-icons.js
@@ -1,10 +1,12 @@
 export default {
-    icon_home: "svg/menu/icon-home.svg", 
-    icon_home_color: "svg/menu/icon-home-color.svg",
-    icon_create: "svg/menu/icon-create.svg", 
-    icon_create_color: "svg/menu/icon-create-color.svg",
-    icon_voted: "svg/menu/icon-voted.svg", 
-    icon_voted_color: "svg/menu/icon-voted-color.svg",
-    icon_help: "svg/menu/icon-help.svg", 
-    icon_help_color: "svg/menu/icon-help-color.svg"
-}
+	icon_open: 'svg/menu/icon-open.svg',
+	icon_open_color: 'svg/menu/icon-open-color.svg',
+	icon_concluded: 'svg/menu/icon-concluded.svg',
+	icon_concluded_color: 'svg/menu/icon-concluded-color.svg',
+	icon_create: 'svg/menu/icon-create.svg',
+	icon_create_color: 'svg/menu/icon-create-color.svg',
+	icon_voted: 'svg/menu/icon-voted.svg',
+	icon_voted_color: 'svg/menu/icon-voted-color.svg',
+	icon_help: 'svg/menu/icon-help.svg',
+	icon_help_color: 'svg/menu/icon-help-color.svg'
+};
