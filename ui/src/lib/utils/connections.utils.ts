e1d62e21248227c0917a591f62eca8ad9fb32c8b
kels7 <hiketabo@gmail.com>
2023-04-17 23:41:20 +0000

    add error check for returned txn results

--- ui/src/lib/utils/connections.utils.ts
+++ ui/src/lib/utils/connections.utils.ts
@@ -68,9 +68,11 @@ export const handleTxnInfo = (txInfo: any) => {
 	} else {
 		// const { title, returnResult } = resultInfo;
 		if (txBlockResult && Object.keys(txBlockResult).length > 0) {
-			const { result, stamps_used } = txBlockResult;
+			const { errors, result, stamps_used } = txBlockResult;
 			if (result === 'None') {
 				sendToastMessageSuccess('Txn Success', `Success! \nstamps used: ${stamps_used}`);
+			} else if (errors[0] !== undefined) {
+				sendToastMessageError('Txn Error', `${errors[0]}`);
 			} else if (result.includes('Error')) {
 				sendToastMessageError('Txn Error', `${result} \nstamps used: ${stamps_used}`);
 			}
