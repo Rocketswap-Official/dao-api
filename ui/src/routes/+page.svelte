540795b4a2a6a4994d38f30f16446f0b1c95225f
kels7 <hiketabo@gmail.com>
2023-04-17 23:42:30 +0000

    check for counting stage for proposal

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -3,20 +3,30 @@
     import ChoiceSelectorPieChart from '$lib/components/choiceSelectorPieChart.svelte';
     import ProposalCard from '$lib/components/proposalCard.svelte';
     import Button from '$lib/components/button/button.svelte'
-    import type { I_Proposal, Tuple7 } from '../lib/types/imported-types'
+    import type { I_Proposal, I_Choice,Tuple7 } from '../lib/types/imported-types'
     import { handle_modal_open_voting, handle_modal_open_details } from '../events'
-    import { countTxnInfo } from '../config';
+    import { countTxnInfo, verifyTxnInfo } from '../config';
     import { handleTxnInfo } from '$lib/utils/connections.utils';
-    import { lwc_store, toast_store} from '$lib/store';
+    import { lwc_store, toast_store, wallet_store} from '$lib/store';
+    import { isAnyProposalCounted } from '../lib/utils/api.utils';
 
     export let data
 
-    const spitDateObj = (proposalArray: I_Proposal): Date => {
-        const dateArray: Tuple7<number> = proposalArray.date_decision.__time__
-        return new Date(...dateArray)
+    const getComputedDate = (proposalArray: I_Proposal): Date => {
+        let dateArray: Tuple7<number> = proposalArray.date_decision.__time__
+        //decrement month to display correctly
+        dateArray[1] -= 1 
+        const utcDate = new Date(Date.UTC(...dateArray))
+        return utcDate
     }
 
-    const getCurrentTime = (): Date => {
+    const getActualDate = (proposalArray: I_Proposal): Date => {
+        let dateArray: Tuple7<number> = proposalArray.date_decision.__time__
+        const utcDate = new Date(Date.UTC(...dateArray))
+        return utcDate
+    }
+
+    const getCurrentDate = (): Date => {
         return new Date()
     }
 
@@ -26,33 +36,48 @@
         $lwc_store.sendTransaction(countTxnInfo, handleTxnInfo)
     }
 
+    const hasUserVoted = (choiceObj: I_Choice[]): boolean => {
+        for(let c of choiceObj){
+           if (c.vk === $wallet_store ){
+                return true
+           }
+        }
+        return false
+    }
+
+    
+
 </script>
 
 <div style="display: grid; 
     grid-template-columns:repeat(auto-fit, minmax(500px, 1fr)); 
     grid-gap: 20px">
     
-    {#if Object.keys(data.proposals).length > 0}
+    {#if Object.keys(data.proposals).length > 0 && !isAnyProposalCounted(data.proposals)}
         {#each data.proposals as proposal}
-            
-            <ProposalCard {proposal}  endDate = {spitDateObj(proposal)}> 
+           
+            <ProposalCard {proposal}  endDate = {getComputedDate(proposal)}> 
                 
                 <ChoiceSelectorPieChart choices ={data.choiceArray[proposal.proposal_id - 1]}/>
 
                 <div class="flex row j-end" style="margin-top: 3vw;">
 
-                    {#if getCurrentTime() > spitDateObj(proposal)}
+                    {#if getCurrentDate() > getActualDate(proposal)}
                         <div class="mr-1em">
                             <Button id={proposal.proposal_id}  act = {()=>submitCountTxn(proposal.proposal_id)} style="">
-                               Count
+                            Count
                             </Button>
                         </div>
                     {:else}
-                        <div class="mr-1em">
-                            <Button id={proposal.proposal_id}  act = {handle_modal_open_voting} style="">
-                            Vote
-                            </Button>
-                        </div>
+
+                        {#if !hasUserVoted(data.choiceArray[proposal.proposal_id - 1])}
+                            <div class="mr-1em">
+                                <Button id={proposal.proposal_id}  act = {handle_modal_open_voting} style="">
+                                Vote
+                                </Button>
+                            </div>
+                        {/if}
+                        
                     {/if}
                     
                     <Button id={proposal.proposal_id} act = {handle_modal_open_details} style="">
@@ -62,7 +87,7 @@
                 </div>
                 
             </ProposalCard>
-            
+    
         {/each}
 
     {:else}
