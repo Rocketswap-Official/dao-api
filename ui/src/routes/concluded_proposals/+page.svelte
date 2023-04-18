47c573bcbc55c336e36a623b3f979b3ee318802e
kels7 <hiketabo@gmail.com>
2023-04-18 21:24:17 +0000

    add a spinner to page

--- ui/src/routes/concluded_proposals/+page.svelte
+++ ui/src/routes/concluded_proposals/+page.svelte
@@ -19,30 +19,50 @@
   
 </script>
 
-<div style="display: grid; 
-    grid-template-columns:repeat(auto-fit, minmax(500px, 1fr)); 
-    grid-gap: 20px">
-    
-    {#if Object.keys(data.proposals).length > 0}
-        {#each data.proposals as proposal}
-            
-            <ProposalCard {proposal} endDate = {getComputedDate(proposal)}> 
+{#if data === undefined}
+    <div class="loader">
+        <img style="width: 55px" src="Rolling-1s-200px.gif" alt="spinner"/>
+    </div>
+
+{:else}
+
+    <div style="display: grid; 
+        grid-template-columns:repeat(auto-fit, minmax(500px, 1fr)); 
+        grid-gap: 20px">
+        
+        {#if Object.keys(data.proposals).length > 0}
+            {#each data.proposals as proposal}
                 
-                <ChoiceSelectorPieChart choices ={data.choiceArray[proposal.proposal_id - 1]}/>
+                <ProposalCard {proposal} endDate = {getComputedDate(proposal)}> 
+                    
+                    <ChoiceSelectorPieChart choices ={data.choiceArray[proposal.proposal_id - 1]}/>
 
-                <div class="flex row j-end" style="margin-top: 3vw;">
-                    <Button id={proposal.proposal_id} act = {handle_modal_open_details} style="">
-                        Details
-                    </Button>
-                </div>
+                    <div class="flex row j-end" style="margin-top: 3vw;">
+                        <Button id={proposal.proposal_id} act = {handle_modal_open_details} style="">
+                            Details
+                        </Button>
+                    </div>
+                    
+                </ProposalCard>
                 
-            </ProposalCard>
-            
-        {/each}
+            {/each}
 
-    {:else}
-            <p style="color: red; font-size: 11px;">No concluded proposals at the moment</p>
-    {/if}
-</div>
+        {:else}
+                <p style="color: red; font-size: 11px;">No concluded proposals at the moment</p>
+        {/if}
+    </div>
 
+{/if}
 
+<style>
+    .loader{
+        position: fixed;
+        height: 100%;
+        width: 100%;
+    }
+    img{
+        position: relative;
+        left: 40%;
+        top: 40%;
+    }
+</style>
