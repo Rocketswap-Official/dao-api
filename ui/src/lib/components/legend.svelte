9c6c7c533ad71d8b959f8376d8463fcccfab6d0c
kels7 <hiketabo@gmail.com>
2023-04-19 21:30:12 +0000

    add legend

---
+++ ui/src/lib/components/legend.svelte
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="flex space-between a-end" >
+        <img width=20px src="svg/checked.svg" alt="selected choice"/>
+        <div>YOUR CHOICE</div>
+    </div>
+       
+    <div class="flex space-between a-end">
+        <img width=30px src="svg/award-medal-badge.svg" alt="leading vote"/>
+        <div>WINNING VOTE</div>
+    </div>
+</div>
+
+<style>
+    .container{
+        width: 120px;
+        /* border: solid 2px red ; */
+    }
+    .margin-l-0{
+        margin-left: 0;
+    }
+</style>
