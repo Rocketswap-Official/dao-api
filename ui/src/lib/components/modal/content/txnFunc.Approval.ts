f3f453e75e267eb7cd11a6b714be4adfddf2897b
kels7 <hiketabo@gmail.com>
2023-04-20 20:21:47 +0000

    add txn callback func to handle counting txn

---
+++ ui/src/lib/components/modal/content/txnFunc.Approval.ts
@@ -0,0 +1,34 @@
+import {
+	sendToastMessageSuccess,
+	sendToastMessageError,
+	closeToast
+} from '../../../../lib/utils/connections.utils';
+import { wallet_store } from '../../../../lib/store';
+import { get } from 'svelte/store';
+import { getApprovalBalance } from '../../../../lib/utils/api.utils';
+
+export const handleTxnInfo = async (txInfo: any) => {
+	//handle txn results
+	const { data } = txInfo;
+	const { errors, txBlockResult } = data;
+	if (errors) {
+		sendToastMessageError('Txn Error', errors[0]);
+	} else {
+		// const { title, returnResult } = resultInfo;
+		if (txBlockResult && Object.keys(txBlockResult).length > 0) {
+			const { errors, result, stamps_used } = txBlockResult;
+			if (result === 'None') {
+				sendToastMessageSuccess('Txn Success', `Success! \nstamps used: ${stamps_used}`);
+				// update RSWP approved amount
+				await getApprovalBalance(get(wallet_store));
+			} else if (errors[0] !== undefined) {
+				sendToastMessageError('Txn Error', `${errors[0]}`);
+			} else if (result.includes('Error')) {
+				sendToastMessageError('Txn Error', `${result} \nstamps used: ${stamps_used}`);
+			}
+		}
+	}
+	setTimeout(() => {
+		closeToast();
+	}, 3000);
+};
---
+++ ui/src/routes/txnFunc.ts
@@ -0,0 +1,34 @@
+import {
+	sendToastMessageSuccess,
+	sendToastMessageError,
+	closeToast
+} from '../lib/utils/connections.utils';
+import { reloadOpenProposalPage } from './+page.svelte';
+
+export const handleTxnInfo = async (txInfo: any) => {
+	//handle txn results
+	const { data } = txInfo;
+	const { errors, txBlockResult } = data;
+	if (errors) {
+		sendToastMessageError('Txn Error', errors[0]);
+	} else {
+		// const { title, returnResult } = resultInfo;
+		if (txBlockResult && Object.keys(txBlockResult).length > 0) {
+			const { errors, result, stamps_used } = txBlockResult;
+			if (result === 'None') {
+				sendToastMessageSuccess('Txn Success', `Success! \nstamps used: ${stamps_used}`);
+				// reload data when txn is successful
+				setTimeout(() => {
+					reloadOpenProposalPage();
+				}, 2000);
+			} else if (errors[0] !== undefined) {
+				sendToastMessageError('Txn Error', `${errors[0]}`);
+			} else if (result.includes('Error')) {
+				sendToastMessageError('Txn Error', `${result} \nstamps used: ${stamps_used}`);
+			}
+		}
+	}
+	setTimeout(() => {
+		closeToast();
+	}, 2500);
+};
